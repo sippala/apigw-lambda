9314cffd9a0fd11e90c075f63a2acfdc0b2dc32c
Shankar Ippala <ishan753@gmail.com>
2019-12-23 10:11:18 -0700

    Add files via upload

---
+++ modules/sgs/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  default = " "
+}
+
+variable "description" {
+  default = " "
+}
+
+variable "vpc_id" { 
+
+}
+
+variable "ingress" { type = "map" } 
+variable "egress"  { type = "map" } 
